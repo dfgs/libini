5a83d136673be4a3229267f53185513928e3ead5
dfgs <dfgs@0dfca2f1-e371-4d5a-a319-849fb7afa777>
2016-07-15 19:17:28 +0000

    M    ini.h M    ini.c

--- ini.h
+++ ini.h
@@ -1,39 +1,42 @@
 #ifndef _INIFILE_
 #define _INIFILE_
 
-typedef struct keyValuePair keyValuePair;
-struct keyValuePair
+typedef struct 
 {
    char* key;
    char* value;
-};
+} keyValuePair;
 
 
-typedef struct section section;
-struct section
+typedef struct
 {
     char* name;
     int currentAllocation;
     int count;
     keyValuePair *items;
-};
+} section; 
 
-typedef struct ini ini;
-struct ini
+
+typedef struct 
 {
     int currentAllocation;
     int count;
     section *items;
-};
+} ini;
 
 ini* ini_open(const char* fileName);
 void ini_free(ini* ini);
 void ini_dump(ini* ini);
-char* ini_getString(ini *ini, const char* section,const char* key,char* defaultValue);
-int ini_getInt(ini *ini, const char* section,const char* key,int defaultValue);
-unsigned short ini_getUnsignedShort(ini *ini, const char* sectionName,const char* keyName,unsigned short defaultValue);
-section* ini_getSection(ini *ini, const char* name);
-keyValuePair* ini_getKeyValuePair(section *section, const char* name);
+
+char* ini_getString(ini *ini, const char* sectionName,const char* key,char* defaultValue);
+int ini_getInt(ini *ini, const char* sectionName,const char* key,int defaultValue);
+unsigned short ini_getUnsignedShort(ini *ini, const char* sectionName,const char* key,unsigned short defaultValue);
+
+void ini_setString(ini *ini,const char* sectionName,const char* key,const char* value);
+
+
+section* ini_getSection(ini *ini, const char* nameName);
+keyValuePair* ini_getKeyValuePair(section *section, const char* key,char* defaultValue);
 
 
 
